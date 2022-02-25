58f4f2d8d62f838a182937c102719437acc940d6
Ulyana-AA <andreeva.ulyanakpc572020@gmail.com>
2022-02-25 22:09:50 +0300

    м

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
-import os
+import gspread
+from datetime import date
 
-from kivy.app import App
-from kivy.lang.builder import Builder
-from kivy.uix.screenmanager import ScreenManager
-
-from screens.welcome import Welcome
-from screens.clients import Clients
-
-
-class MembershipAccounting(App):
-
-    def build(self):
-        sm = ScreenManager()
-
-        Builder.load_file(os.path.join(os.getcwd(), 'designs', 'welcome.kv'))
-        sm.add_widget(Welcome(name='welcome'))
-
-        Builder.load_file(os.path.join(os.getcwd(), 'designs', 'clients.kv'))
-        sm.add_widget(Clients(name='clients'))
-
-        return sm
+gs = gspread.service_account(filename='abon.json')  # подключаем файл с ключами и пр.
+sh = gs.open_by_key('19LBStaP31DZLTPpLtqqMGXo2mPYUN5roy_68uIUI_3o')  # подключаем таблицу по ID
+worksheet = sh.sheet1  # получаем первый лист
+#  res = worksheet.get_all_records()
+#  res = worksheet.get_all_values()
+#  print(res)
+#  ['идентификатор абонемента', 'клиента', 'дата создания', 'цена', 'кол-во занятий', 'пройденных занятий',
+#  ' дата и время последней тренировки']
 
 
-if __name__ == '__main__':
-    MembershipAccounting().run()
+today = date.today()  # дата через тире/дефисы
+v = "{}.{}.{}".format(today.day, today.month, today.year)  # дата через точки
+
+newRec = ['506', '203', v, '500 руб.', '2', '0', '']  # новые данные
+
+#  worksheet.insert_row(newRec, 2)  # добавляет новые данные во 2 строку
+worksheet.append_row(newRec)
